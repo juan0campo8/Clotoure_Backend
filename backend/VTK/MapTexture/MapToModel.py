fd826f008e46f1a1ccaaa03fc123eb29f946ece2
Adil Chaka <achaka@stevens.edu>
2024-03-26 15:43:15 -0400

    Merging Teddy MapToModel.py changes

--- backend/VTK/MapTexture/MapToModel.py
+++ backend/VTK/MapTexture/MapToModel.py
@@ -15,12 +15,19 @@ from vtkmodules.vtkIOImage import vtkPNGReader
 from vtkmodules.vtkIOGeometry import vtkOBJReader
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleTrackballCamera
 from vtkmodules.vtkInteractionWidgets import vtkCameraOrientationWidget
+from vtkmodules.vtkRenderingAnnotation import vtkAxesActor
+
+from vtkmodules.vtkFiltersCore import (
+    vtkPolyDataTangents,
+    vtkTriangleFilter
+)
 
 from vtkmodules.vtkFiltersSources import (
     vtkCubeSource,
     vtkParametricFunctionSource,
     vtkTexturedSphereSource
 )
+
 from vtkmodules.vtkRenderingCore import (
     vtkActor,
     vtkPolyDataMapper,
@@ -38,53 +45,73 @@ def get_program_parameters():
     epilogue = '''
    '''
     parser = argparse.ArgumentParser(description=description, epilog=epilogue, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('filename1', help='masonry-wide.jpg.')
-    parser.add_argument('filename2', help='tshirt.obj')
+    parser.add_argument('filename1', help='shirtfront.jpg.')
+    parser.add_argument('filename2', help='shirtback.jpg')
+    parser.add_argument('filename3', help='tshirt.obj')
     args = parser.parse_args()
     arg1 = args.filename1
     arg2 = args.filename2
-    return args.filename1, args.filename2
+    arg3 = args.filename3
+    return args.filename1, args.filename2, args.filename3
     # takes in jpg file and obj file IN THAT ORDER
-    
-# def rotate_callback(key,actor): 
-#    if key == "Left":
-#        actor.RotateY(5.0)
-#    elif key == "Right":
-#        actor.RotateY(-5.0)
-#    elif key == "Up":
-#        actor.RotateX(5.0)
-#    elif key == "Down":
-#         actor.RotateX(-5.0)
         
+def create_window(actor):
+    ren = vtkRenderer()
+    render_window = vtkRenderWindow()
+    render_window.SetSize(480, 480)
+    render_window.AddRenderer(ren)
+    
+    render_window_interactor = vtkRenderWindowInteractor()
+    render_window_interactor.SetRenderWindow(render_window)
+    
+    ren.AddActor(actor)
+    ren.ResetCamera()
+    
+    return render_window, render_window_interactor
+
 
 def main():
     colors = vtkNamedColors()
-    jpegfile, objfile = get_program_parameters()
+    jpegfile, jpegfile2, objfile = get_program_parameters()
     
     # back_jpeg = get_program_parameters() ##implement 2nd texture reading 
     
-    #jpegfile = "./res/8k_earth_daymap.jpg"
-    #objfile  = "./obj/tshirt.obj"
+    #jpegfile  = "./res/8k_earth_daymap.jpg"
+    #jpegfile2 = "./res/BackShirt.jpg"
+    #objfile   = "./obj/tshirt.obj"
+    
+    jpegfile = "./res/" + jpegfile
+    jpegfile2 = "./res/" + jpegfile2
+    objfile = "./obj/" + objfile
     
     # Create a render window
     ren = vtkRenderer()
     renWin = vtkRenderWindow()
     renWin.AddRenderer(ren)
     renWin.SetSize(480, 480)
-    renWin.SetWindowName('Model Render')
+    renWin.SetWindowName('Render0')
+    
+    ren1 = vtkRenderer()
+    renWin1 = vtkRenderWindow()
+    renWin1.AddRenderer(ren1)
+    renWin1.SetSize(480, 480)
+    renWin1.SetWindowName('Render1')
 
     iren = vtkRenderWindowInteractor()
+    iren1 = vtkRenderWindowInteractor()
+
     
-    
-    # Set render window
+    # Set render windows
     iren.SetRenderWindow(renWin)
+    iren1.SetRenderWindow(renWin1)
 
     # Read the image data from a file
+    
     reader = vtkJPEGReader()
     reader.SetFileName(jpegfile)
     
     reader2 = vtkJPEGReader()
-    # reader2.SetFileName()
+    reader2.SetFileName(jpegfile2)
     
     # read the obj data from a file
     objreader = vtkOBJReader()
@@ -92,18 +119,18 @@ def main():
 
     # Create texture object
     texture = vtkTexture()
+    texture.InterpolateOn()
     texture.SetInputConnection(reader.GetOutputPort())
     
     texture2 = vtkTexture()
-    texture2.SetInputConnection(reader.GetOutputPort())  # Second texture 
+    texture.InterpolateOn()
+    texture2.SetInputConnection(reader2.GetOutputPort())  # Second texture 
 
     # Map texture coordinates
     
     map_to_model = vtkTextureMapToPlane()   #Plane texture map is good
     # UV Bias for shifting image in various directions
-    
-    # map_to_model = vtkTextureMapToCylinder()      Cylinder texture map is also good
-    # map_to_model = vtkTextureMapToSphere()
+
     map_to_model.SetInputConnection(objreader.GetOutputPort())
     # map_to_model.PreventSeamOn()
 
@@ -112,36 +139,71 @@ def main():
     mapper.SetInputConnection(map_to_model.GetOutputPort())
 
     # Create actor and set the mapper and the texture
-    actor = vtkActor()
+    
     bp = vtkProperty()
-    bp.SetColor(0,1,0)
+    bp.SetColor(colors.GetColor3d('Blue'))
     # actor.GetProperty().SetColor(colors.GetColor3d('red'))
-    actor.SetMapper(mapper)
-    actor.SetTexture(texture)
-    actor.SetBackfaceProperty(bp)
+   
+    # Create first actor
+    actor1 = vtkActor()
+    actor1.SetMapper(mapper)
+    
+    # Create second actor
+    actor2 = vtkActor()
+    actor2.SetMapper(mapper)
+    
+    
+    #Axes
+    axes = vtkAxesActor()
+    
 
-    ren.AddActor(actor)
-    ren.SetBackground(colors.GetColor3d('White'))
-    # Interactor 
-    # interactorStyle = vtkInteractorStyleTrackballCamera()
-    # iren.SetInteractorStyle(interactorStyle)
+    texture.SetWrap(vtkTexture.ClampToEdge)
+
+
+    actor1.SetTexture(texture)
+    actor1.SetBackfaceProperty(bp)
+    
+    actor2.SetTexture(texture2)
+    actor2.SetBackfaceProperty(bp)
+    
+    actor1.SetPosition(0,0,5)
+    actor2.SetPosition(0,0,-0.5)
+    # actor2.RotateY(180) ## Rotate Actor
+    
+    renderWindow1, renderWindowInteractor1 = create_window(actor1)
+    
+    ren.AddActor(actor1)
+    ren.AddActor(actor2)
+    ren.SetBackground(colors.GetColor3d('Red'))
+    
+    ren1.AddActor(actor2)
+    ren1.SetBackground(colors.GetColor3d('Blue'))
+    
+    ren.AddActor(axes)
+    ren1.AddActor(axes)
+    
+    
+    renderWindow1, renderWindowInteractor1 = create_window(actor1)
 
     iren.Initialize()
-    
-    # key = vtkRenderWindowInteractor.GetKeySym()
-    # vtkRenderWindowInteractor.AddObserver("KeyPressEvent", rotate_callback(key, actor))
+    # iren1.Initialize()
     
     cam_orient_manipulator = vtkCameraOrientationWidget()
     cam_orient_manipulator.SetParentRenderer(ren)
+    # cam_orient_manipulator.SetParentRenderer(ren1)
     # Enable the widget.
     cam_orient_manipulator.On()
     
+    
+    
+    
     renWin.Render()
+    # renWin1.Render()
     iren.Start()
+    # iren1.Start()
     
     
 
 if __name__ == '__main__':
     
     main()
-
